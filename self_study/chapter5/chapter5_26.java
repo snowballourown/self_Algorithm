3c21ad65e7f476ddb2dad68ffbfc6c777567639b
jun200 <82015977+snow1000000000000000000@users.noreply.github.com>
2026-03-08 14:13:37 +0900

    N+M reply

--- self_study/chapter5/chapter5_26.java
+++ self_study/chapter5/chapter5_26.java
@@ -1,10 +1,55 @@
 package self_study.chapter5;
 
-public class chapter5_26 {
-    static int [] A;
-    static boolean visited [];
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    public static void main(String[] args) {
+public class chapter5_26 {
+    static boolean[] visited;
+    static int[] array;
+    static int M;
+    static int N;
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stn = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(stn.nextToken());
+        M = Integer.parseInt(stn.nextToken());
+        visited = new boolean[N];
+        array = new int[N];
+        backtracking(0);
 
     }
+
+
+    private static void backtracking(int length) {
+        if (length == M) {
+            printArray();
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+
+                visited[i] = true;
+                array[length] = i;  // 해당 배열에다가 i를 넣어버림
+                backtracking(length + 1);
+                visited[i] = false;
+
+            }
+        }
+    }
+
+
+
+    private static void printArray() {
+        for (int i = 0; i < M; i++) {
+            System.out.println(array[i] + 1 + " ");
+        }
+    }
+
 }
--- self_study/chapter5/chapter5_26re1.java
+++ self_study/chapter5/chapter5_26re1.java
@@ -1,4 +1,63 @@
 package self_study.chapter5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class chapter5_26re1 {
+    static boolean[] visited;
+    static int[] array;
+    static int M; //배열 크기
+    static int N; // 숫자 수
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stn = new StringTokenizer(br.readLine(), " ");
+
+         N = Integer.parseInt(stn.nextToken());
+         M = Integer.parseInt(stn.nextToken());
+
+        array = new int[M];
+        visited = new boolean[N];
+
+         backtracking(0
+         );
+
+    }
+
+
+    static void backtracking(int length) {
+        if (length == M) {
+            PrintArray();
+            return;
+        }
+
+
+        for (int i = 0; i < N; i++) {
+
+            if (!visited[i])
+            {
+                visited[i] = true;
+                array[length] = i;
+                backtracking(length+1);
+                visited[i] = false;
+            }
+
+
+        }
+
+
+
+
+    }
+
+
+    static void PrintArray() {
+        for (int i : array) {
+            System.out.print(i +1 + " ");
+
+
+        }
+        System.out.println("");
+    }
 }
