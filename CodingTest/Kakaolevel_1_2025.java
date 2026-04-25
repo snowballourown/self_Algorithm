bc3fe2948f153a6f50c3894b48354d87d0f3def5
jun200 <82015977+snow1000000000000000000@users.noreply.github.com>
2026-04-25 16:44:12 +0900

    kakao 2025 개어렵다

---
+++ CodingTest/Kakaolevel_1_2025.java
@@ -0,0 +1,52 @@
+
+
+class Solution {
+
+    int gcd(int a, int b) {  // 두 수를 동시에 나눌 수 있는 가장 큰 수  //최대 공약수찾기
+        while (b != 0) {
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+
+    int lcm(int a, int b) { //두 주기가 다시 동시에 맞는 최소 시간 = 최소 공배수
+        return a / gcd(a, b) * b;  //  : a / gcd =>   a의 공약수를 뺸 값을 구함   b(b의 서로소   * 최소공약수 )
+    }
+
+
+    int solution(int[][] signals) {
+        int n = signals.length;
+
+        int lcmVal = 1;
+        for (int i = 0; i < n; i++) {
+            int cycle = signals[i][0] + signals[i][1] + signals[i][2];
+            lcmVal = lcm(lcmVal, cycle); // 계속 여기에 저장되면서 다음값을 받으면서 같은 최소공배수값들을 구함
+        }
+
+        for (int t = 1; t <= lcmVal; t++) { // 최소공배수값들을 구해서 그값만 딱하고 빠짐
+            boolean allYellow = true;
+
+            for (int i = 0; i < n; i++) {
+                int G = signals[i][0];
+                int Y = signals[i][1];
+                int R = signals[i][2];
+
+                int cycle = G + Y + R; // 해당 값의 사이클을 저장
+                int time = (t - 1) % cycle + 1; //사이클 초기화 시켜주는과정
+
+                if (!(time > G && time <= G + Y)) {
+                    allYellow = false;
+                    break;
+                }
+            }
+
+            if (allYellow) return t;
+        }
+
+        return -1;
+
+    }
+}
+    
--- self_study/mentoringafter/P1919.java
+++ self_study/mentoringafter/P1919.java
@@ -6,39 +6,37 @@ import java.io.InputStreamReader;
 
 public class P1919 {
     public static void main(String[] args) throws IOException {
+        // 각 배열들을 받고 아스키 코드에 맞게 저장
+        // 배열 2개를 받아서 -를 하셈 나오는게 2개 각자나오거나 업생
+        // 결과값이 0은 없으니까 안해도되고,
+        // 1 같은 숫자들이나오면 한개밖에 안나온거니까 신경안써도 됨
+        // 0은 ㄱㅊ
 
-        
-        // 두문자열 비교해서 서로 하나씩 제거하면서 문자열들을 비교해야겠다
-        // 2초 -> 이중 for문 까지는 가능
-        // 배열을  2개 다 받고
-        // 겹치는거 스펠링만 체크하면되는거아니야>? 갯수나?
-        // 나머지는 다제거해도되고?
-        // 그 제거한 갯수를 출력하는거고
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] A = br.readLine().split("");
-        String[] B = br.readLine().split("");
-
-        int num[][] = new int[2][26];
 
 
-        for (String s : A) {
-            num[0][s.charAt(0) - 'a']++;
+        String[] a = br.readLine().split("");
+        String[] b = br.readLine().split("");
+
+        int [][] num1 = new int[2][26];
+
+
+        for (int i = 0; i < a.length; i++) {
+            num1[0][a[i].charAt(0) - 'a']++;
+        }
+        for (int i = 0; i < b.length; i++) {
+            num1[1][b[i].charAt(0) - 'a']++;
         }
 
-
-        for (String s : B) {
-            num[1][s.charAt(0) - 'a']++;
-        }
-
-int cnt = 0;
-
-
-        for (int i = 0; i < 26; i++) {
-            cnt += Math.abs(num[0][i] - num[1][i]);
-        }
-
+        int cnt = 0 ;
+            for (int j = 0; j < 26; j++) {
+                cnt += Math.abs(num1[0][j] - num1[1][j]);
+            }
 
         System.out.println(cnt);
+
+
+
 
     }
 }
