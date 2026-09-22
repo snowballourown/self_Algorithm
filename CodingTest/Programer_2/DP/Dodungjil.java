d25a76c6b18df9a11fd73309f720532165657ed2
dlagudwns <dlagudwns30@naver.com>
2026-09-22 09:44:16 +0900

    gogo
    # Conflicts:
    #	CodingTest/Programer_2/DP/Dodungjil.java

--- CodingTest/Programer_2/DP/Dodungjil.java
+++ CodingTest/Programer_2/DP/Dodungjil.java
@@ -1,30 +1,27 @@
 package CodingTest.Programer_2.DP;
 
-public class Dodungjil{
-    public int solution(int[] money) {
-        int answer = 0;
-        int  n = money.length;
+public class Dodungjil {
 
-        return Math.max(rub(money, 0, n - 2), rub(money, 1, n - 1));
+
+    public int solution(int[] money) {
+
+        int n = money.length;
+
+        return Math.max(rob(money, 0, n-2), rob(money, 1, n-1));
     }
 
-    private int rub(int[] money, int start, int end) {
 
-        int pre1 = 0 ;
-        int pre2 = 0 ;
+    private int rob(int[] money, int start, int end) {
+        int prev2 = 0;
+        int prev1 = 0;
 
         for (int i = start; i <= end; i++) {
-
-            int current = Math.max(pre1, pre2 + money[i]);
-            pre2 = pre1;
-            pre1 = current;
+            int current = Math.max(prev2, prev1 + money[i]);
+            prev2 = prev1; //전위치 -> 전전위치
+            prev1 = current; // 현재위치 -> 전위치
 
 
         }
-
-
-        return pre1;
+        return prev1;
     }
 }
-
-
